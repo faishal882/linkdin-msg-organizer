5ec8e14861492bf680bb95c4fcaad0d3d26ceb9f
faishal <faishalmanzar@gmail.com>
2025-03-13 00:45:47 +0530

    Improved UI of loading screen and error screen

--- src/components/loadingscreen.tsx
+++ src/components/loadingscreen.tsx
@@ -1,10 +1,11 @@
+// src/components/loadingscreen.tsx
 import "./stylesheets/loadingscreen.css";
 
 const LoadingScreen = () => {
   return (
     <div className="loading-container">
       <div className="loader"></div>
-      <p className="loading-text">Loading messages...</p>
+      <p className="loading-text">Scraping messages...</p>
     </div>
   );
 };
